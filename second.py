5a219d9c5e12db2e0d85998e50c58b4378fe0d18
terrabayteee <blak__ang@mail.ru>
2017-09-07 20:44:45 +0300

    second task

--- second.py
+++ second.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
     df2 = pd.read_excel("Analyst_Data.xls", sheetname=2, header=None)
     df1 = df1.drop(0, axis=0)
     df1.columns = ['col1', 'col2']
-    df1.corr()
+    print(df1.col1.astype('float64').corr(df1.col2.astype('float64'), method='pearson'))
+    df2 = df2.drop(0, axis=0)
+    df2.columns = ['col1', 'col2']
+    print(df2.col1.astype('float64').corr(df2.col2.astype('float64'), method='pearson'))
+
